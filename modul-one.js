f7db6db38a4aaa973be1e06d66d7dfbdebe552b0
NPC-Roman <ispacebobo@gmail.com>
2023-06-12 23:11:37 +0300

    Initial commit

---
+++ modul-one.js
@@ -0,0 +1,13 @@
+// Change code below this line
+
+// console.log(productName);
+const productName = 'Droid';
+
+// console.log(pricePerItem);
+const pricePerItem = 2000;
+
+const message = `${productName} = ${pricePerItem}`;
+
+console.log(productName);
+console.log(pricePerItem);
+console.log(message);
