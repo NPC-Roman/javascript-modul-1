4b33190be32391f48faf93c34bcd18dc7ea9bfba
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-06-20 17:03:23 +0300

    Ch7

--- modul-two.js
+++ modul-two.js
@@ -1,7 +1,7 @@
 //---------//
 //         //
 //---------//
-const colors = ['#47d147', '#ff1a1a', 'pink', '#80b3ff', 'orchid', '#1ad1ff']; //['candy', 'merigold', 'mantis', 'baby blue', 'orchid', 'rouge'];
+const colors = ['#47d147', '#ff8080', 'pink', '#80b3ff', 'orchid', '#1ad1ff']; //['candy', 'merigold', 'mantis', 'baby blue', 'orchid', 'rouge'];
 const max = colors.length - 1;
 const min = 0;
 
@@ -37,19 +37,144 @@ document.body.style.backgroundColor = color;
 //---------//
 //----03---//
 //---------//
-function checkStorage(available, ordered) {
-    // Change code below this line
-    let message;
+/*function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return "Your order is empty!";
+  } 
+  if (ordered > available) {
+    return "Your order is too large, not enough goods in stock!";
+  }   
+    return "The order is accepted, our manager will contact you";
+}
+console.log(checkStorage(150, 0));*/
+//---------//
+//----04---//
+//---------//
+//---------------===//////////////////===-------------//
+/*const fruits = ["apple", "plum", "pear", "orange"];*/
+//---------//
+//----05---//
+//---------//
+/*const fruits = ["apple", "plum", "pear", "orange"];
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[3];
+console.log(firstElement);*/
+//---------//
+//----06---//
+//---------//
+/*const fruits = ["apple", "plum", "pear", "orange"];
+console.log(fruits)
+
+fruits[1] = "peach";
+fruits[3] = "banana";
+console.log(fruits)*/
+//---------//
+//----07---//
+//---------//
+/*const fruits = ["apple", "peach", "pear", "banana"];
+
+const fruitsArrayLength = fruits.length;
+console.log(`Довжина масиву: ${fruitsArrayLength}`);*/
+//---------//
+//----08---//
+//---------//
+/*const fruits = ["apple", "peach", "pear", "banana"];
+
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[lastElementIndex];
+
+console.log(lastElementIndex);
+console.log(lastElement);
+console.log(`Фрукт з номером ${lastElementIndex} називається ${lastElement}`);*/
+//---------//
+//----09---//
+//---------//
+/*function getExtremeElements(array) { 
+  let miniMass
+
+  const firstInName = array[0];
+  const lastIn = array.length - 1;
+  const lastInName = array[lastIn];
+ 
+  console.log(firstInName);
+  console.log(lastIn);
+  console.log(lastInName);
+
+  return miniMass = [firstInName, lastInName];
+}
+console.log(getExtremeElements(["Earth", "Mars", "Venus"]));*/
+//---------//
+//----10---//
+//---------//
+/*function splitMessage(message, delimiter) {
+  let words;
+
+  words = message.split(delimiter);
+
+  return words;
+}
+console.log(splitMessage("Mango hurries to the train", " "));*/
+//---------//
+//----11---//
+//---------//
+/*function calculateEngravingPrice(message, pricePerWord) {
+  let totalPrice
   
-    if (ordered === 0) {
-      message = "Your order is empty!";
-    } else if (ordered > available) {
-      message = "Your order is too large, not enough goods in stock!";
-    } else {
-      message = "The order is accepted, our manager will contact you";
-    }
+  const calArr = message.split(" ");
+  const numLett = calArr.length;
+  console.log(calArr);
+  console.log(calArr.length);
+  console.log(calArr.length);
+
+  totalPrice = numLett * pricePerWord;
+  return totalPrice;
+}
+
+calculateEngravingPrice("JavaScript is in my blood", 10);
+console.log(calculateEngravingPrice("Web-development is creative work", 40));*/
+//---------//
+//----12---//
+//---------//
+/*function makeStringFromArray(array, delimiter) {  
+  let string;
+
+  string = array.join(delimiter);
+
+  return string;
+}
+console.log(makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " "));*/
+//---------//
+//----13---//
+//---------//
+/*function slugify(title) {
+
+  let slugDone;
+  sluMakeLen = title.toLowerCase();
   
-    return message;
-    // Change code above this line
+  sluMakeArr = sluMakeLen.split(" ");
+  
+  sluMakeJoy = sluMakeArr.join("-");
+  
+  return slugDone = sluMakeJoy
+  
   }
-  
+  console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));*/
+//---------//
+//----14---//
+//---------//
+/*const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+const firstTwoEls = fruits.slice(0, 2);
+console.log(firstTwoEls);
+const nonExtremeEls = fruits.slice(1, 4);
+console.log(`масив з усіх елементів, крім першого та останнього: ${nonExtremeEls}`);
+const lastThreeEls = fruits.slice(-3);*/
+//---------//
+//----15---//
+//---------//
+const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+const newClients = ['Peach', 'Houston'];
+
+const allClients = oldClients.concat(newClients);
+console.log(allClients);
