38b23dee5df8f8b5787826b30091923939d48039
NPC-Roman <ispacebobo@gmail.com>
2023-06-24 21:08:27 +0300

    Ch9

--- modul-two.js
+++ modul-two.js
@@ -256,3 +256,160 @@ return longWorld;*/
 //---------//
 //----22---//
 //---------//
+/*function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+    for (let item = min; item <= max; item += 1) {    
+    numbers.push(item)
+}
+  console.log(numbers)
+  // Change code above this line
+  return numbers;
+}
+createArrayOfNumbers(1, 3);
+console.log(createArrayOfNumbers(29, 34));*/
+//---------//
+//----23---//
+//---------//
+/*function filterArray(numbers, value) {
+    // Change code below this line
+    /*const newArr = [];
+ console.log(numbers.length);
+ for (let item = 0; numbers[item] > value; item += 1) {    
+   newArr.push(numbers[item]);
+   
+}
+ return newArr;
+ console.log(newArr);*/
+/*const newArr = [];
+
+    for (let item = 0; item < numbers.length; i += 1) {
+        console.log(numbers.length);
+        if (numbers[item] > value) {
+            newArr.push(numbers[item]);
+        }
+    }
+    return newArr;
+    // Change code above this line
+}
+filterArray([1, 2, 3, 4, 5], 3);
+console.log(filterArray([1, 2, 3, 4, 5], 3));*/
+//---------//
+//----24---//
+//---------//
+/*function filterArray(numbers, value) {
+    // Change code below this line
+    /*const newArr = [];
+ console.log(numbers.length);
+ for (let item = 0; numbers[item] > value; item += 1) {    
+   newArr.push(numbers[item]);
+   
+}
+ return newArr;
+ console.log(newArr);*/
+/*const newArr = [];
+
+    for (let item of numbers) {
+        if (item > value) {
+            newArr.push(item);
+        }
+    }
+    return newArr;
+    // Change code above this line
+}
+filterArray([1, 2, 3, 4, 5], 3);
+console.log(filterArray([1, 2, 3, 4, 5], 3));*/
+//---------//
+//----25---//
+//---------//
+/*function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit); // Change this line
+}*/
+//---------//
+//----26---//
+//---------//
+function getCommonElements(array1, array2) {
+    // Change code below this line
+    let newArr = [];
+    for (let item = 0; item < array1.length; item += 1) {
+        if (array2.includes(array1[item])) {
+            newArr.push(array1[item]);
+        }
+    }
+    return newArr;
+    // Change code above this line
+}
+//---------//
+//----  ---//
+//---------//
+function calculateTotalPrice(order) {
+    let total = 0;
+    // Change code below this line
+
+    for (const items of order) {
+        total += items;
+    }
+
+    /*for (let i = 0; i < order.length; i += 1) {
+    total += order[i];
+     console.log(total)
+  }
+*/
+    // Change code above this line
+    return total;
+}
+calculateTotalPrice([12, 85, 37, 4]);
+//---------//
+//----  ---//
+//---------//
+// Change code below this line
+const a = 3 % 1;
+const b = 4 % 3;
+const c = 11 % 4;
+const d = 12 % 7;
+const e = 8 % 6;
+//---------//
+//----  ---//
+//---------//
+function getEvenNumbers(start, end) {
+    // Change code below this line
+    const newArr = [];
+    for (let item = start; item <= end; item += 1) {
+        if (item % 2 === 0) {
+            newArr.push(item);
+        }
+    }
+    return newArr;
+
+    // Change code above this line
+}
+//---------//
+//----  ---//
+//---------//
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+    if (i % 5 === 0) {
+        number = i;
+        break;
+    }
+}
+//---------//
+//----  ---//
+//---------//
+function findNumber(start, end, divisor) {
+    // Change code below this line
+    let number;
+
+    for (let i = start; i < end; i += 1) {
+        if (i % divisor === 0) {
+            return i;
+        }
+    }
+
+    // Change code above this line
+}
